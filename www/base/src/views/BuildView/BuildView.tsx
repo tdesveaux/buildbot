22f571e4d7e32c4a0ecee5e001e61281e034c21b
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2024-05-01 17:32:30 +0200

    www: Use builder name instead of id in `/builders/{builderid}` links

--- www/base/src/views/BuildView/BuildView.tsx
+++ www/base/src/views/BuildView/BuildView.tsx
@@ -150,6 +150,8 @@ const BuildView = observer(() => {
   const buildersQuery = useDataApiQuery(() => Builder.getAll(accessor, {id: builderid}));
   const builder = buildersQuery.getNthOrNull(0);
 
+  const builderUrlId = builder?.name ?? builderid;
+
   const now = useCurrentTime();
 
   // get the build plus the previous and next
@@ -239,9 +241,9 @@ const BuildView = observer(() => {
   const shouldNavigateToBuilder = buildersQuery.isResolved() && buildsQuery.isResolved() && build === null;
   useEffect(() => {
     if (shouldNavigateToBuilder) {
-      navigate(`/builders/${builderid}`);
+      navigate(`/builders/${builderUrlId}`);
     }
-  }, [builderid, navigate, shouldNavigateToBuilder]);
+  }, [builderUrlId, navigate, shouldNavigateToBuilder]);
 
   const responsibleUsers = computed(() => getResponsibleUsers(propertiesQuery, changesQuery)).get();
   /*
