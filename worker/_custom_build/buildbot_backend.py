c880f5710b76e00db85afee3fd810bfe4499fac1
Thomas Desveaux <desveaux.thomas@gmail.com>
2025-05-25 12:59:50 +0200

    buildbot_backend: move NO_INSTALL_REQS behaviour from setup.py
    This will allow to move packages dependencies to pyproject.toml.
    
    NO_INSTALL_REQS no longer remove extra dependencies:
     - it's doable but not as simple as just stripping dependencies
     - did not really make sense, user could simply NOT provide extra to install

--- worker/_custom_build/buildbot_backend.py
+++ worker/_custom_build/buildbot_backend.py
@@ -1,3 +1,23 @@
 # Dummy buildbackend
+from __future__ import annotations
 
-from setuptools.build_meta import *  # type: ignore # noqa: F403
+import os
+
+from setuptools import build_meta as _orig
+from setuptools.build_meta import *  # noqa: F403
+
+
+def _no_install_reqs() -> bool:
+    return bool(os.getenv('NO_INSTALL_REQS'))
+
+
+def get_requires_for_build_sdist(config_settings: _orig._ConfigSettings = None) -> list[str]:
+    if _no_install_reqs():
+        return []
+    return _orig.get_requires_for_build_sdist(config_settings=config_settings)
+
+
+def get_requires_for_build_wheel(config_settings: _orig._ConfigSettings = None) -> list[str]:
+    if _no_install_reqs():
+        return []
+    return _orig.get_requires_for_build_wheel(config_settings=config_settings)
