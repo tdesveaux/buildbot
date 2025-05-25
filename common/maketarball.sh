6199c2c046b405fdef4d060fbd6f7f218ab1274d
Thomas Desveaux <desveaux.thomas@gmail.com>
2025-05-25 14:31:48 +0200

    common.maketarballs: master/pkg/worker can be built in isolation

--- common/maketarball.sh
+++ common/maketarball.sh
@@ -5,9 +5,9 @@ pkg=$1
     cd ${pkg}
     rm -rf MANIFEST dist
     if [ ${pkg} == "master" ] || [ ${pkg} == "worker" ] || [ ${pkg} == "pkg" ]; then
-        python -m build --no-isolation --sdist
+        python -m build --sdist
         # wheels must be build separately in order to properly omit tests
-        python -m build --no-isolation --wheel
+        python -m build --wheel
     else
         # retry once to workaround instabilities
         python -m build --no-isolation || (git clean -xdf; python -m build --no-isolation)
